6816b5a95e3212220939cc1dc114f64f6e4c5d97
Jason Zopf <jasonzopf@gmail.com>
2014-02-06 09:49:02 -0500

    card scaffold

--- app/controllers/api/v1/cards_controller.rb
+++ app/controllers/api/v1/cards_controller.rb
@@ -1,21 +1,51 @@
 class Api::V1::CardsController < ApplicationController
+  before_action :set_card, only: [:show, :update, :destroy]
   respond_to :json
 
+  # GET /cards
   def index
-    cards = Card.all
-    render json: cards
+    respond_with Card.all
   end
 
+  # GET /cards/1
+  def show
+    respond_with @card
+  end
+
+  # POST /cards
   def create
-    card = Card.new(card_params)
-    if card.save
-      render json: card
+    @card = Card.new(card_params)
+
+    if @card.save
+      respond_with @card, status: :created, location: [:api, :v1, @card]
     else
-    render status: 422
+      render json: @card.errors, status: :unprocessable_entity
     end
   end
 
-  protected
+  # PATCH/PUT /cards/1
+  def update
+    if @card.update(card_params)
+      respond_with @card, status: :ok, location: [:api, :v1, @card]
+    else
+      render json: @card.errors, status: :unprocessable_entity
+    end
+  end
+
+  # DELETE /cards/1
+  def destroy
+    @card.destroy
+    head :no_content
+  end
+
+  private
+
+  # Use callbacks to share common setup or constraints between actions.
+  def set_card
+    @card = Card.find(params[:id])
+  end
+
+  # Never trust parameters from the scary internet, only allow the white list through.
   def card_params
     params.require(:card).permit(:letter)
   end
--- app/serializers/card_serializer.rb
+++ app/serializers/card_serializer.rb
@@ -1,5 +1,3 @@
 class CardSerializer < ActiveModel::Serializer
-  embed :ids
   attributes :id, :letter
-  has_many :options
 end
