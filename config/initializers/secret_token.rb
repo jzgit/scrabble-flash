2bb06570822eb87e59dabd090eed6f74b05d6a3c
Jason Zopf <jasonzopf@gmail.com>
2014-02-05 21:44:37 -0500

    Initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+ScrabbleFlash::Application.config.secret_key_base = '10b0329b89ec3c8b58d064295c8db0bfbb7d68c01ab2c1c587bc356b47898b065e4010aceca1fa392a332f5c3663e6039f47b6bd2c81fa637710ce5b23959f4d'
